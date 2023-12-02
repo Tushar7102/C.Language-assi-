18b28e4ed371ade777c05f9034cfe18a5e589439
Tushar003 <143060635+Tushar7102@users.noreply.github.com>
2023-12-02 08:05:03 +0530

    Add files via upload

---
+++ frist/P1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+  clrscr;
+  printf("\nMY NAME IS:- TUSHAR PATIL\n");
+  printf("MY AGE IS:- 18 YEARS \n");
+  printf("MY SCHOOL NAME IS:- MATRUBHUMI VIDHAY SANKUL \n");
+  getch();
+}
